e82f19a771ca6af731292d403381618d0618314a
Lakmal Ranasinghe <lakmal84@gmail.com>
2014-12-28 13:25:52 +0800

    added some more comments

--- src/edge.cpp
+++ src/edge.cpp
@@ -1,24 +1,21 @@
-//
-//  edge.cpp
-//  TestOpenCV
-//
-//  Created by Lakmal on 20/12/14.
-//  Copyright (c) 2014 NTU. All rights reserved.
-//
-
+/**
+*
+* Copyright (c) 2014 Lakmal Ranasinghe
+* Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+*
+*/
 
 #include <iostream>
+#include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/gpu/gpu.hpp>
-#include <stdio.h>
 
 using namespace std;
 using namespace cv;
 using namespace cv::gpu;
-
 
 timespec diff(timespec start, timespec end)
 {
@@ -34,34 +31,35 @@ timespec diff(timespec start, timespec end)
     return temp;
 }
 
-
 int main(int, char**)
 {
     VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
+    if(!cap.isOpened())  // check if camera open succeeded
         return -1;
-    
+
     GpuMat edges;
-    struct timespec start, end;	
+    struct timespec start, end;
+
     namedWindow("edges",1);
-    for(int i=0; i<20; i++)
+
+    while(1)
     {
-	//clock_gettime(CLOCK_REALTIME, &start); /* mark start time */
-        Mat frame;
-	GpuMat gpu_frame;
-        cap >> frame; // get a new frame from camera
-	gpu_frame.upload(frame);
-        clock_gettime(CLOCK_REALTIME, &start); /* mark start time */
-	gpu::cvtColor(gpu_frame, edges, CV_BGR2GRAY);
+        Mat src_frame;
+        GpuMat gpu_frame;
+        cap >> src_frame; // get a new frame from camera
+        gpu_frame.upload(src_frame); // copy camera frame to GPU
+        clock_gettime(CLOCK_REALTIME, &start); // mark start time 
+
+        gpu::cvtColor(gpu_frame, edges, CV_BGR2GRAY);
         gpu::GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         gpu::Canny(edges, edges, 0, 30, 3);
-	clock_gettime(CLOCK_REALTIME, &end);
-	Mat dst_host(edges);
-        imshow("edges", dst_host);
-	//clock_gettime(CLOCK_REALTIME, &end);
-	printf("Latency = %lu.%lu\n", diff(start, end).tv_sec, diff(start, end).tv_nsec);
+
+        clock_gettime(CLOCK_REALTIME, &end); // mark end time
+        Mat dst_frame(edges);
+        imshow("edges", dst_frame);
+        printf("Execution latency = %lu.%lu\n", diff(start, end).tv_sec, diff(start, end).tv_nsec);
         if(waitKey(200) >= 0) break;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
+
